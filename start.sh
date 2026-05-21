b9ad021e056146f0438d7bd8a71095f7297c4593
Christian A. Dampil <chanskiadolfo@gmail.com>
2026-05-21 16:12:07 +0800

    add docker configuration

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Cache config
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Run migrations
+php artisan migrate --force
+
+# Start PHP-FPM and Nginx
+php-fpm -D
+nginx -g "daemon off;"
